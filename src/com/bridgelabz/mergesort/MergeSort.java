35be9e1d49d7525a944bfead974b7585f9a879f3
kamal14rajput <kamal14rajput@gmail.com>
2021-12-30 23:33:05 +0530

    MODIFIED: prints sorted array

--- src/com/bridgelabz/mergesort/MergeSort.java
+++ src/com/bridgelabz/mergesort/MergeSort.java
@@ -1,51 +1,76 @@
 package com.bridgelabz.mergesort;
+import java.util.Arrays;
 
-public class MergeSort {
-	
-	public static void main(String[] args) {
+public class MergeSort<T extends Comparable<T>> {
 
-		String[] a = { "King", "Panda", "Soldiers", "Dancing", "Flowering","Cat", "Mum-Mum", "Joyfull", "In-out" };
-		String[] b = { "Marvel", "Kitty", "Miracle", "Money", "Rekha","Zoom-zoom", "Crying", "Bubbles", "Amazing", "Rose",
-				"Magnet" };
-		String[] c = new String[a.length + b.length];
-		mergeSort(a);
-		mergeSort(b);
-		merge(c, a, b);
-		mergeSort(c);
-		// Arrays.sort(names);
-		for (String arr : c) {
-			System.out.println(arr);
-		}
+	// merges two subarrays of array[].
+    void merge(T[] array, int start, int middle, int end)
+    {
+        T[] leftArray  = (T[]) new Comparable[middle - start + 1];
+        T[] rightArray = (T[]) new Comparable[end - middle];
+
+        // fill in left array
+        for (int i = 0; i < leftArray.length; ++i)
+            leftArray[i] = array[start + i];
+
+        // fill in right array
+        for (int i = 0; i < rightArray.length; ++i)
+            rightArray[i] = array[middle + 1 + i];
+
+        /* Merge the temp arrays */
+
+        // initial indexes of first and second subarrays
+        int leftIndex = 0, rightIndex = 0;
+
+        // the index we will start at when adding the subarrays back into the main array
+        int currentIndex = start;
+
+        // compare each index of the subarrays adding the lowest value to the currentIndex
+        while (leftIndex < leftArray.length && rightIndex < rightArray.length)
+        {
+            if (leftArray[leftIndex].compareTo(rightArray[rightIndex]) <= 0)
+            {
+                array[currentIndex] = leftArray[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                array[currentIndex] = rightArray[rightIndex];
+                rightIndex++;
+            }
+            currentIndex++;
+        }
+
+        // copy remaining elements of leftArray[] if any
+        while (leftIndex < leftArray.length) 
+        	array[currentIndex++] = leftArray[leftIndex++];
+
+        // copy remaining elements of rightArray[] if any
+        while (rightIndex < rightArray.length) 
+        	array[currentIndex++] = rightArray[rightIndex++];
+    }
+    // main function that sorts array[start..end] using merge()
+    void mergeSort(T[] array, int start, int end)
+    {
+        // base case
+        if (start < end)
+        {
+            // find the middle point
+            int middle = (start + end) / 2;
+
+            mergeSort(array, start, middle); // sort first half
+            mergeSort(array, middle + 1, end);  // sort second half
+
+            // merge the sorted halves
+            merge(array, start, middle, end);
+        }
+    }
+    
+    public static void main(String[] args) {
+    	 String[] arrayOfStrings = {"You","will","never","always","be","motivated.","You","have","to","learn","to","be","disciplined"};
+         MergeSort<String> stringSorter   = new MergeSort<>();
+         stringSorter.mergeSort(arrayOfStrings, 0, arrayOfStrings.length - 1);
+         System.out.println("Array After Sorting\n"+Arrays.toString(arrayOfStrings));
 	}
-
-	public static void mergeSort(String[] c) {
-		if (c.length > 1) {
-			String[] a = new String[c.length / 2];
-			String[] b = new String[c.length - c.length / 2];
-			for (int i = 0; i < a.length; i++) {
-				a[i] = c[i];
-			}
-			for (int j = 0; j < b.length; j++) {
-				b[j] = c[j + c.length / 2];
-			}
-			mergeSort(a);
-			mergeSort(b);
-			merge(c, a, b);// calling merge method
-		}
-	}
-	
-	public static void merge(String[] name, String[] left, String[] right) {
-		int as = 0;
-		int bs = 0;
-		for (int i = 0; i < name.length; i++) {
-			if (bs >= right.length || (as < left.length && left[as].compareToIgnoreCase(right[bs]) < 0)) {
-				name[i] = left[as];
-				as++;
-			} else {
-				name[i] = right[bs];
-				bs++;
-			}
-		}
-
-	}
+    
 }
